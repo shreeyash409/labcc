76fddaf1ee86256696890c31e73a2fa392eed250
Shreeyash Mandage <shreeyashmandage@gmail.com>
2026-05-07 10:42:44 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -4,9 +4,6 @@ def chatbot():
 
 while True:
   user = input ("you:").lower()
-
-  if user = "exit":
-    print("invalid error try again")
   elif "help" in user:
     print("what typeof help i can do?")
 chatbot()
