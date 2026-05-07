f6cf81f5a887d8a947bc023820327337519c69b8
Shreeyash Mandage <shreeyashmandage@gmail.com>
2026-05-07 10:43:28 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,9 +1,4 @@
 print("shreeyash Hello")
 def chatbot():
   print("chatbot: hello!!!how can 9i help you ?\n")
-
-while True:
-  user = input ("you:").lower()
-  elif "help" in user:
-    print("what typeof help i can do?")
 chatbot()
